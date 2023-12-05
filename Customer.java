ebc46f30673c395c74fb1557c3c1728305c76248
xzg5101 <guoxiaoyang11@outlook.com>
2023-12-05 15:01:21 -0500

    feat: account classes

--- Customer.java
+++ Customer.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 public class Customer extends Account{
-    
+    private ArrayList<Stock> stocks;
+    public Customer(String userName, String password){
+        super(userName, password);
+        this.stocks = new ArrayList<Stock>();
+    }
 }
--- Manager.java
+++ Manager.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+
 public class Manager extends Account {
-    
+    public Manager (String userName, String password){
+        super(userName, password);
+    }
 }
