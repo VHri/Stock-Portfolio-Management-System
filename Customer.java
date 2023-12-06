46082521e1bff196ef8b1788a735a62610912e23
xzg5101 <guoxiaoyang11@outlook.com>
2023-12-06 12:46:24 -0500

    feat: add compute values in customer class

--- Customer.java
+++ Customer.java
@@ -9,6 +9,47 @@ public class Customer extends Account{
         this.stocks = new ArrayList<Stock>();
     }
 
+    public double computeTotalValue(){
+        double totalValue = 0.0;
+        for (Stock s: stocks){
+            totalValue += s.getTotalValue();
+        }
+        return totalValue;
+    }
+
+    
+
+    public void buyStock(Stock stock){
+        for(Stock s: stocks){
+            if (s.getName() == stock.getName()){
+                s.setCount(s.getCoutnt()+stock.getCoutnt());
+                s.setTotalValue(s.getTotalValue() + stock.getTotalValue());
+                break;
+            }
+        }
+
+        stocks.add(stock);
+    }
+
+    public boolean sellStock(Stock stock, int count){
+        for(Stock s: stocks){
+            if (s.getName() == stock.getName()){
+                if (s.getCoutnt()<count){
+                    System.err.printf("Customer: stock count not enough");
+                    return false;
+                } else {
+                    s.setCount(s.getCoutnt()-stock.getCoutnt());
+                    s.setTotalValue(s.getTotalValue() - (double)count*stock.getPrice());
+                    return true;
+                }
+            }
+        }
+
+        System.err.printf("Customer: stock not found");
+        return false;
+    }
+
+    // getters and setters
     public void setStocks(ArrayList<Stock> stocks){
         this.stocks = stocks;
     }
