719e110aeff44db4298bf609cb4af3c48eab8319
xzg5101 <xzg5101@bu.edu>
2023-12-07 14:32:51 -0500

    feat: add identity verification

---
+++ Constant.java
@@ -0,0 +1,7 @@
+public class Constant {
+    public static final int APPROVED_USER = 1001;
+    public static final int UNAPPROVED_USER = 1002;
+    public static final int MANAGER = 1003;
+    public static final int UNKNOW_USER = 1004;
+
+}
